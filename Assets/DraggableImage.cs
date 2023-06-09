5ebbacc1b06a4e5b93a117fda4db623734340fbd
KubisBarb <82451125+KubisBarb@users.noreply.github.com>
2023-06-09 19:54:22 +0200

    File checkpoint

--- Assets/DraggableImage.cs
+++ Assets/DraggableImage.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
@@ -11,6 +12,9 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
     private Vector2 minDragPosition;
     private Vector2 maxDragPosition;
 
+    bool focusingOnLocation = true;
+    public RectTransform focusPointLocation;
+
     private void Awake()
     {
         imageRectTransform = GetComponent<RectTransform>();
@@ -20,6 +24,61 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
     private void Start()
     {
         CalculateDragBounds();
+    }
+
+    Vector3 RecalculateFocusPosition()
+    {
+        // Get the local position of the RectTransform
+        Vector2 localPosition = focusPointLocation.localPosition;
+
+        // Convert the local position to world position
+        Vector3 worldPosition = focusPointLocation.TransformPoint(localPosition);
+
+        return worldPosition;
+    }
+
+    Vector3 RecalculateIconPosition()
+    {
+        // Puts pointer click position into worlds space
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Convert mouse position to world space
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        return worldPosition;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw gizmos for the focus position
+        Gizmos.color = Color.red;
+        Vector3 focusPosition = RecalculateFocusPosition();
+        Gizmos.DrawSphere(focusPosition, 0.1f);
+
+        // Draw gizmos for the icon position
+        Gizmos.color = Color.green;
+        Vector3 iconPosition = RecalculateIconPosition();
+        Gizmos.DrawSphere(iconPosition, 0.1f);
+
+        // Draw a line between focus and icon positions
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(focusPosition, iconPosition);
+    }
+
+    private void Update()
+    {
+        if (focusingOnLocation)
+        {
+            // Calculate the vector from the original location to the desired location, wanted - original
+            Vector3 offset = RecalculateFocusPosition() - RecalculateIconPosition();
+
+            // Move the object instantly to the desired location
+            imageRectTransform.position += offset;
+
+            Debug.Log(offset);
+
+            focusingOnLocation = false;
+        }
     }
 
     private void CalculateDragBounds()
@@ -57,4 +116,10 @@ public class DraggableImage : MonoBehaviour, IPointerDownHandler, IDragHandler
 
         imageRectTransform.localPosition = newPosition;
     }
+
+    public void FocusOnLocation()
+    {
+        focusingOnLocation = true;
+    }
+
 }
